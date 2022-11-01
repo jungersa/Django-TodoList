7f87959316703f2187c7da8381a7aa3803e19a67
jungersa <Arnaud.jungers@gmail.com>
2022-11-01 15:52:57 +0100

    add the todo.task model

--- app/todo/models.py
+++ app/todo/models.py
@@ -1,3 +1,49 @@
-from django.db import models
+"""This modules contains all the models used by the todo app"""
 
-# Create your models here.
+from django.db import models
+from django.urls import reverse
+from app.utils.models import BaseModel
+
+
+class Tasks(BaseModel):
+    """A model which represents taks
+
+    Args:
+        name (str): Is the task name: required
+        description (str): Is the task description: optional
+        due_date (date): Is the task due date: optional
+        state (bool): Completion state of the task
+
+    """
+    name = models.CharField(
+        "task name",
+        max_length=25,
+        blank=False,
+        help_text="Is the task name: required",
+    )
+    description = models.CharField(
+        "task description",
+        max_length=150,
+        null=False,
+        blank=True,
+        help_text="Is the task description: optional",
+    )
+    due_date = models.DateField(
+        "task due date",
+        null=True,
+        blank=True,
+        help_text="Is the task due date: optional",
+    )
+    state = models.BooleanField(
+        "task state",
+        default=False,
+        help_text="Completion state of the task",
+    )
+
+    def __str__(self) -> str:
+        """Return the Task string representation"""
+        return self.name
+
+    def get_absolute_url(self) -> str:
+        """Return the url related to self"""
+        return reverse("todo:detail", kwargs={"pk": self.pk})
