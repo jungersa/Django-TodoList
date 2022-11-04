5be09a19eeb59e1ddd391cca0ca6a3d2091d953e
Arnaud Jungers <arnaud.jungers@gmail.com>
2022-11-04 18:02:09 +0100

    Adding the project model

--- app/todo/models.py
+++ app/todo/models.py
@@ -2,32 +2,52 @@
 
 from django.db import models
 from django.urls import reverse
-from app.utils.models import BaseModel
+
+from app.utils.models import NameDescModel
+
+# Create your models here.
 
 
-class Tasks(BaseModel):
+class Project(NameDescModel):
+    """A project which stores a list of tasks
+
+    A project with a name, description and a list of tasks. Related to :model: `app.todo.Task`
+
+    Args:
+        name (str): Is the project name: required (max_lenght 50)
+        description (str): Is the project description: optional
+        tasks (Task): a foreign key in the :model: `app.todo.Task`
+
+    """
+
+    class Meta:
+        verbose_name = "project"
+        verbose_name_plural = "projects"
+
+    def __str__(self) -> str:
+        """Return the representation of self"""
+        return self.name
+
+    def get_absolute_url(self) -> str:
+        """Return the url related to self Project (Project detail page)"""
+        return reverse("todo:project_detail", kwargs={"pk": self.pk})
+
+    def todo_restante(self) -> int:
+        return self.tasks.filter(state=False).count()
+
+
+class Task(NameDescModel):
     """A model which represents taks
 
     Args:
-        name (str): Is the task name: required
+        name (str): Is the task name: required (max_lenght 50)
         description (str): Is the task description: optional
         due_date (date): Is the task due date: optional
         state (bool): Completion state of the task
+        project (Project): The :model: `app.todo.Project` to which the task is related
 
     """
-    name = models.CharField(
-        "task name",
-        max_length=25,
-        blank=False,
-        help_text="Is the task name: required",
-    )
-    description = models.CharField(
-        "task description",
-        max_length=150,
-        null=False,
-        blank=True,
-        help_text="Is the task description: optional",
-    )
+
     due_date = models.DateField(
         "task due date",
         null=True,
@@ -39,11 +59,17 @@ class Tasks(BaseModel):
         default=False,
         help_text="Completion state of the task",
     )
+    project = models.ForeignKey(
+        "todo.Project",
+        null=True,
+        on_delete=models.CASCADE,
+        related_name='tasks',
+    )
 
     def __str__(self) -> str:
         """Return the Task string representation"""
         return self.name
 
     def get_absolute_url(self) -> str:
-        """Return the url related to self"""
+        """Return the url related to self Task (Task detail page)"""
         return reverse("todo:detail", kwargs={"pk": self.pk})
